e77f00f5f9bd63928882d4971fdc20626a6d0ce7
Bartek thindil Jasicki <thindil@laeran.pl>
2019-10-03 07:56:05 +0200

    made pylinit bit more happier

--- adadocset.py
+++ adadocset.py
@@ -19,21 +19,21 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import re, sqlite3
+"""Build Ada 2012 specification docset from the HTML version of it."""
 
-conn = sqlite3.connect('Ada.docset/Contents/Resources/docSet.dsidx')
-cur = conn.cursor()
-try:
-    cur.execute('DROP TABLE searchIndex;')
-except:
-    pass
-cur.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
+import re
+import sqlite3
+
+CONN = sqlite3.connect('Ada.docset/Contents/Resources/docSet.dsidx')
+CUR = CONN.cursor()
+CUR.execute('DROP TABLE searchIndex;')
+CUR.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
 
 # Packages, Types, Subprograms, Objects
-filenames = ["RM-Q-1.html", "RM-Q-2.html", "RM-Q-3.html", "RM-Q-4.html", "RM-Q-5.html"]
-types = ["Package", "Type", "Function", "Exception", "Object"]
-for j in range(len(filenames)):
-    with open("Ada.docset/Contents/Resources/Documents/" + filenames[j]) as fn:
+FILENAMES = ["RM-Q-1.html", "RM-Q-2.html", "RM-Q-3.html", "RM-Q-4.html", "RM-Q-5.html"]
+TYPES = ["Package", "Type", "Function", "Exception", "Object"]
+for j in range(len(FILENAMES)):
+    with open("Ada.docset/Contents/Resources/Documents/" + FILENAMES[j]) as fn:
         content = fn.readlines()
     i = 0
     while i < len(content):
@@ -47,32 +47,34 @@ for j in range(len(filenames)):
                 line = content[i].strip()
             line = line[line.find("<A HREF=") + 9:]
             path = line[:line.find("\"")]
-            cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, types[j], path))
+            CUR.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)',
+                        (name, TYPES[j], path))
         i += 1
 
 #Pragmas
 with open("Ada.docset/Contents/Resources/Documents/RM-L.html") as fn:
-    content = fn.readlines()
+    CONTENT = fn.readlines()
 i = 0
-while i < len(content):
-    line = content[i].strip()
-    if line.find("<B>pragma</B>") > 0:
-        while content[i].find("</div>") == -1:
-            line += content[i].strip()
+while i < len(CONTENT):
+    LINE = CONTENT[i].strip()
+    if LINE.find("<B>pragma</B>") > 0:
+        while CONTENT[i].find("</div>") == -1:
+            LINE += CONTENT[i].strip()
             i += 1
-        line += content[i].strip()
-        result = re.search(">\w+(\[|\(|;)", line)
-        name = line[result.span()[0] + 1 : result.span()[1] - 1]
-        line = line[line.find("See <A HREF=") + 13:]
-        path = line[:line.find("\"")]
-        cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, "Directive", path))
+        LINE += CONTENT[i].strip()
+        RESULT = re.search(">\w+(\[|\(|;)", LINE)
+        NAME = LINE[RESULT.span()[0] + 1 : RESULT.span()[1] - 1]
+        LINE = LINE[LINE.find("See <A HREF=") + 13:]
+        PATH = LINE[:LINE.find("\"")]
+        CUR.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)',
+                    (NAME, "Directive", PATH))
     i += 1
 
 #Aspects and Attributes
-filenames = ["RM-K-1.html", "RM-K-2.html"]
-types = ["Property", "Attribute"]
-for j in range(len(filenames)):
-    with open("Ada.docset/Contents/Resources/Documents/" + filenames[j]) as fn:
+FILENAMES = ["RM-K-1.html", "RM-K-2.html"]
+TYPES = ["Property", "Attribute"]
+for j in range(len(FILENAMES)):
+    with open("Ada.docset/Contents/Resources/Documents/" + FILENAMES[j]) as fn:
         content = fn.readlines()
     i = 0
     while i < len(content):
@@ -86,24 +88,26 @@ for j in range(len(filenames)):
             line = content[i].strip()
             line = line[line.find("See <A HREF=") + 13:]
             path = line[:line.find("\"")]
-            cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, types[j], path))
+            CUR.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)',
+                        (name, TYPES[j], path))
         i += 1
 
 # Chapters
 with open("Ada.docset/Contents/Resources/Documents/RM-TOC.html") as fn:
-    content = fn.readlines()
+    CONTENT = fn.readlines()
 i = 0
-start = False
-while i < len(content):
-    line = content[i].strip()
-    if line == "<HR>":
-        start = not start
-    if start and line.find("<A HREF=") > -1:
-        line = line[line.find("<A HREF=") + 9:]
-        path = line[:line.find("\"")]
-        name = line[line.find("\"") + 2:line.find("</A>")]
-        cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, "Section", path))
+START = False
+while i < len(CONTENT):
+    LINE = CONTENT[i].strip()
+    if LINE == "<HR>":
+        START = not START
+    if START and LINE.find("<A HREF=") > -1:
+        LINE = LINE[LINE.find("<A HREF=") + 9:]
+        PATH = LINE[:LINE.find("\"")]
+        NAME = LINE[LINE.find("\"") + 2:LINE.find("</A>")]
+        CUR.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)',
+                    (NAME, "Section", PATH))
     i += 1
 
-conn.commit()
-conn.close()
+CONN.commit()
+CONN.close()
